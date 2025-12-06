4291caed8945e30ed7d17871f929d848784775c7
harshvaland-del <er.harsh26@gmail.com>
2025-12-06 12:04:38 +0530

    Add files via upload

---
+++ Q3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num, first, last;
+
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    last = num % 10;  
+    first = num;       
+    while (first >= 10) {  
+        first = first / 10;   
+    }
+    
+    int sum = first + last ;
+    printf("The sum of first and the last: %d", sum);
+}
